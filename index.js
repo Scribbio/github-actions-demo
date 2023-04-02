f2ff7d04453db077af082385feff27c556b809d6
Scribbio <joeditrolio@outlook.com>
2023-04-03 00:37:36 +0100

    add second route

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ app.get("/test", (_req, res) => {
   res.status(200).send("Hello world");
 });
 
+app.get("/Goodbye", (_req, res) => {
+  res.status(200).send("GoodBye world");
+});
+
 app.listen(port, () => console.log("Example app listening on port 3000!"));
